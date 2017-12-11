f130d6705b8a76f65f40479813f93a563f1367b1
a13821190779 <945039036@qq.com>
2017-12-11 18:57:44 +0800

    弄弄ssr吧

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+	console.log('1')
+	await next()
+	console.log(2)
+})
+
+app.use(async ctx => {
+	await new Promise((resolve, reject) => {
+		setTimeout(function() {
+			resolve()
+		}, 2000)
+	})
+	ctx.body = 'Hello World'
+})
+
+app.listen(3000, () => {
+	console.log('listening....')
+})
